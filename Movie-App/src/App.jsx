f185124b9e77c5aa7f00e6442aad44bc4efb2ac2
hassan0341 <hassan200354@outlook.com>
2025-04-10 12:33:40 +0100

    add header

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="/hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  );
 };
 
 export default App;
